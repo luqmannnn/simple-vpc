ba9a1e30a469cff2f5d7e1eb41fdb2d106aa62be
luqmannnn <luqmannurhakimt@gmail.com>
2025-08-04 20:48:42 +0800

    Update provider

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,13 +8,14 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_vpn_gateway = true
+  enable_vpn_gateway      = true
+  map_public_ip_on_launch = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
-    Created_by = "Admin"
-    Cohort = "CE11"
+    Created_by  = "Admin"
+    Cohort      = "CE11"
   }
 }
 
