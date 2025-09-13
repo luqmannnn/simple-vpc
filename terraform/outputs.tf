0897ff9bb16cc879a96fb1f4050c02231937d7b0
luqmannnn <luqmannurhakimt@gmail.com>
2025-09-13 10:41:37 +0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
 output "vpc_id" {
   value = module.vpc_coaching.vpc_id
 }
-
-output "alb_listener_arn" {
-  value = aws_lb_listener.web_app.arn
-}
-
-output "alb_dns_name" {
-  value = "http://${module.alb.lb_dns_name}"
-}
